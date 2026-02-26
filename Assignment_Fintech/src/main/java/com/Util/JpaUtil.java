cceda0e0ff701b62918c3bdb3a695055a8f36865
Chiliveru Abhiram <chiliveruabhiram.20@gmail.com>
2026-02-26 22:16:43 +0530

    26-02-26

---
+++ Assignment_Fintech/src/main/java/com/Util/JpaUtil.java
@@ -0,0 +1,20 @@
+package com.Util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("fintech-pu");
+
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+	
+	public static void close() {
+		emf.close();
+	}
+	
+
+}
